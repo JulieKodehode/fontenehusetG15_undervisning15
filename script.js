2b4fa5eae4994c0d48444a39e0959b3210fda857
JulieKodehode <julie.kodehode@gmail.com>
2025-09-30 12:02:45 +0200

    After lesson

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-// Hente et element = Hente
-const hamburgerImg = document.querySelector("#hamburgerImg");
+// H = Henter noe
+const submitButton = document.querySelector("#submitButton");
+console.log(submitButton);
 
-// Alternativ 1:
-// Ha en funksjon = Modifiserer
-// Laget en funksjon som henter navList-elementet og bytter klassenavn for synlig/usynlig
-function hamburgerToggle() {
-  console.log("Hi from hamburgerToggle");
+// M = Modifiserer noe
+function gatherData() {
+  console.log("Button clicked?");
 
-  const navListElement = document.querySelector(".navList"); // Bevist brukt klasse for kun et element med det klassenavnet
-  console.log(navListElement);
+  // H = Hente elementer
+  const greetingInput = document.querySelector("#greetingInput");
+  const greetingInputValue = greetingInput.value;
+  console.log(greetingInput);
+  console.log(greetingInputValue);
 
-  // Metode 1: Skrive CSS i JS som blir inline-CSS
-  navListElement.style.display = "flex";
-  navListElement.style.flexDirection = "column";
+  const numberInput = document.querySelector("#numberInput").value;
+  console.log(numberInput);
+
+  const displayFormInputs = document.querySelector("#displayFormInputs");
+
+  // L = Lage elementer
+  const displayGreeting = document.createElement("p");
+
+  // M = Modifisere elementer
+  displayFormInputs.textContent = greetingInputValue;
+  displayFormInputs.textContent = numberInput;
+  displayFormInputs.textContent = greetingInputValue + " " + numberInput;
+  displayFormInputs.textContent = `${greetingInputValue} ${numberInput}`;
+
+  displayGreeting.textContent = greetingInputValue;
+
+  // S = Sende elementer
+  displayFormInputs.appendChild(displayGreeting);
 }
 
-// Aktiverer en funksjon = Sender
-hamburgerImg.addEventListener("click", hamburgerToggle);
+// S = Sender noe
+submitButton.addEventListener("click", gatherData);
